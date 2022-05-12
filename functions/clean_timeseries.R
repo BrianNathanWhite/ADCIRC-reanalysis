5025b22dff1651871f885b2db50d058953ab0b05
Brian Nathan White <briannathanwhite@pop-os.localdomain>
2022-05-12 18:31:43 -0400

    Created file dedicated to preliminary EDA. Separated the two-stage-model implementations according to the order of the second stage mle vector (i.e. by location vs by gaussian process).

--- functions/clean_timeseries.R
+++ functions/clean_timeseries.R
@@ -5,7 +5,7 @@ clean_timeseries <- function(timeseries, metadata) {
   timeseries$TIME <- as.POSIXct(timeseries$TIME, format = "%Y-%m-%d %H:%M:%S")
   
   # create year and month variables from TIME column
-  timeseries <- add_column(timeseries,  year = format(timeseries$TIME, '%Y'), month = format(timeseries$TIME,    '%m'), .after = 1)
+  timeseries <- add_column(timeseries,  year = format(timeseries$TIME, '%Y'), month = format(timeseries$TIME,    '%m'),  .after = 1)
   
   # rename columns with station name
   for(station in metadata$stationname){
